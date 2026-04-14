e699cf23e22c17d73cf5da54d4466e879fad89c8
swap-mitra <swpnlmitra@gmail.com>
2026-04-14 18:49:06 +0530

    iterator adapter

--- src/bin/15-iterators.rs
+++ src/bin/15-iterators.rs
@@ -1,11 +1,22 @@
 fn main() {
-    let mut v1 = vec![1, 2, 3];
-    let v1_iter = v1.iter_mut();
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
 
-    for value in v1_iter {
-        *value = *value * 2;
-        println!("Got: {value}");
+    // iterator adapter
+    let v1_iter2 = v1_iter.map(|x| x * 2);
+
+    for i in v1_iter2 {
+        println!("{}", i);
     }
+
+    // let sum: i32 = v1_iter.sum();
+    // println!("Sum is {sum}");
+
+    //iter_mut impl
+    // for value in v1_iter {
+    //     *value = *value * 2;
+    //     println!("Got: {value}");
+    // }
 
     println!("Array: {:?}", v1)
 }
