6c4a80bd90a12e1c78c92202ea79eaad16b53f15
swap-mitra <swpnlmitra@gmail.com>
2026-05-17 00:44:33 +0530

    lifetimes, threads and message passing

---
+++ src/bin/20-threads.rs
@@ -0,0 +1,32 @@
+use std::thread;
+
+fn main() {
+    thread::spawn(|| {
+        let mut c = 0;
+        for _i in 0..500000000 {
+            for _j in 0..500000000 {
+                c = c + 1;
+                c = c - 1;
+            }
+        }
+    });
+
+    thread::spawn(|| {
+        let mut c = 0;
+        for _i in 0..500000000 {
+            for _j in 0..500000000 {
+                c = c + 1;
+                c = c - 1;
+            }
+        }
+    });
+
+    let mut c = 0;
+    for _i in 0..500000000 {
+        for _j in 0..500000000 {
+            c = c + 1;
+            c = c - 1;
+            // println!("hi from spawned thread {}", i);
+        }
+    }
+}
